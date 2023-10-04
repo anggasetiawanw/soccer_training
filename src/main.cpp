abc0d9c563071854fc7be9c4285874500cb1ed52
anggasetiawanw <anggasetiaw@gmail.com>
2023-10-04 15:24:48 +0700

    Release 1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -26,16 +26,21 @@ const char CODE_ANGLE_VERTICAL = 'C';
 const char CODE_RELEASE = 'E';
 const char CODE_TOGGLE = 'F';
 const char CODE_RESET = 'G';
+const char CODE_MIN_PWM = 'H';
+const char CODE_MAX_PWM = 'I';
 
 // ACCEL DRIVER DEFINE
 #define RUN_SPEED 0
 #define MAX_SPEED 5000
 #define MAX_ACCEL 5000
-#define ZERO_VERTI -40000
-#define CENTER_VERTI 23000
 
-#define ZERO_HORI -18200
-#define CENTER_HORI 9100
+#define ZERO_VERTI -60000
+#define CENTER_VERTI 14000
+#define MAX_VERTI 28000
+
+#define ZERO_HORI -25000
+#define CENTER_HORI 9750
+#define MAX_HORI 19500
 
 AccelStepper angleAtasBawah(AccelStepper::FULL2WIRE, 8, 9);
 AccelStepper angleSamping(AccelStepper::FULL2WIRE, 10, 11);
@@ -43,40 +48,67 @@ int lastPositionHori = 0;
 int lastPositionVerti = 0;
 
 #define RELEASE_PIN 6
+#define ISR_PIN_HORI 2
+#define ISR_PIN_VERTI 3
 
 // CONST DEFINE
 #define MAX_ANALOG 255
 #define MAX_PERCENT 100
 #define MAX_STEP_APPS 200
-#define STEP_HORI 10
-#define STEP_VERTI 75
+
+int MIN_PWM = 45;
+int MAX_PWM = 155;
 
 bool isAnalog = false;
-// Pins
-const byte Analog_X_pin = A0;  // x-axis readings
-const byte Analog_Y_pin = A1;  // y-axis readings
-const byte Analog_R_pin = A2;  // r-axis readings
-
-// Variables
-
-int Analog_R_Value = 0;  // this is used for the PWM value
-
 struct dataLog {
     char code;
     int data;
 };
-void resetPosition() {
-    angleAtasBawah.moveTo(ZERO_VERTI);
-    angleAtasBawah.runToPosition();
+
+void stopsHori() {
+    angleSamping.stop();
+    angleSamping.setCurrentPosition(0);
+    Serial.println("Current HORI : " + String(angleSamping.currentPosition()));
+    detachInterrupt(digitalPinToInterrupt(ISR_PIN_HORI));
+}
+void stopsVerti() {
+    angleAtasBawah.stop();
     angleAtasBawah.setCurrentPosition(0);
-    angleAtasBawah.moveTo(CENTER_VERTI);
-    angleAtasBawah.runToPosition();
-    angleSamping.moveTo(ZERO_HORI);
-    angleSamping.runToPosition();
-    angleSamping.setCurrentPosition(0);
-    angleSamping.moveTo(CENTER_HORI);
-    angleSamping.runToPosition();
-    angleSamping.setCurrentPosition(0);
+    Serial.println("Current VERTI : " + String(angleAtasBawah.currentPosition()));
+    detachInterrupt(digitalPinToInterrupt(ISR_PIN_VERTI));
+}
+void resetPosition() {
+    Serial.println("Detect : " + String(digitalRead(ISR_PIN_HORI)));
+    if (digitalRead(ISR_PIN_HORI) == 0) {
+        angleSamping.setCurrentPosition(0);
+        angleSamping.moveTo(CENTER_HORI);
+        angleSamping.runToPosition();
+    } else {
+        attachInterrupt(digitalPinToInterrupt(ISR_PIN_HORI), stopsHori, LOW);
+        angleSamping.move(ZERO_HORI);
+        while (angleSamping.distanceToGo() < 0) {
+            angleSamping.run();
+        }
+        angleSamping.moveTo(CENTER_HORI);
+        angleSamping.runToPosition();
+        Serial.println("INITIAL POSITION HORIZONTAL : " + String(angleSamping.currentPosition()));
+    }
+
+    Serial.println("Detect VERTI : " + String(digitalRead(ISR_PIN_VERTI)));
+    if (digitalRead(ISR_PIN_VERTI) == 0) {
+        angleAtasBawah.setCurrentPosition(0);
+        angleAtasBawah.moveTo(CENTER_VERTI);
+        angleAtasBawah.runToPosition();
+    } else {
+        attachInterrupt(digitalPinToInterrupt(ISR_PIN_VERTI), stopsVerti, LOW);
+        angleAtasBawah.move(ZERO_VERTI);
+        while (angleAtasBawah.distanceToGo() < 0) {
+            angleAtasBawah.run();
+        }
+        angleAtasBawah.moveTo(CENTER_VERTI);
+        angleAtasBawah.runToPosition();
+        Serial.println("INITIAL POSITION VERTICAL : " + String(angleAtasBawah.currentPosition()));
+    }
 }
 
 void btTask(void *pvParameters) {
@@ -88,7 +120,7 @@ void btTask(void *pvParameters) {
         if (bluetooth.available() > 0) {
             char command = bluetooth.read();
             Serial.println("BT READ : " + String(command));
-            if (command == CODE_SPEED_LEFT || command == CODE_SPEED_RIGHT || command == CODE_ANGLE_VERTICAL || command == CODE_ANGLE_HORIZONTAL || command == CODE_RELEASE || command == CODE_TOGGLE || command == CODE_RESET) {
+            if (command == CODE_SPEED_LEFT || command == CODE_SPEED_RIGHT || command == CODE_ANGLE_VERTICAL || command == CODE_ANGLE_HORIZONTAL || command == CODE_RELEASE || command == CODE_TOGGLE || command == CODE_RESET || command == CODE_MIN_PWM || command == CODE_MAX_PWM) {
                 buffer.code = command;
                 valid = true;
             } else if (command == '\n' && valid) {
@@ -117,7 +149,7 @@ void actTask(void *pvParameters) {
             switch (buffer.code) {
             case CODE_SPEED_LEFT: {
                 if (buffer.data != 0)
-                    buffer.data = map(buffer.data, 0, 200, 45, 155);
+                    buffer.data = map(buffer.data, 0, 200, MIN_PWM, MAX_PWM);
                 if (lastValueSpeedLeft <= buffer.data) {
                     for (int i = lastValueSpeedLeft; i < buffer.data; i++) {
                         analogWrite(SPEED_LEFT, i);
@@ -137,7 +169,7 @@ void actTask(void *pvParameters) {
             }
             case CODE_SPEED_RIGHT: {
                 if (buffer.data != 0)
-                    buffer.data = map(buffer.data, 0, 200, 45, 155);
+                    buffer.data = map(buffer.data, 0, 200, MIN_PWM, MAX_PWM);
                 if (lastvalueSpeedRight < buffer.data) {
                     for (int i = lastvalueSpeedRight; i < buffer.data; i++) {
                         analogWrite(SPEED_RIGHT, i);
@@ -158,54 +190,35 @@ void actTask(void *pvParameters) {
             }
 
             case CODE_ANGLE_HORIZONTAL: {
-                int run = 0;
+                // int run = 0;
                 if (buffer.data > MAX_STEP_APPS) {
                     buffer.data = (buffer.data - MAX_STEP_APPS) * -1;
                 }
-                if (lastPositionHori != 0) {
-                    run = (lastPositionHori - buffer.data);
-                } else {
-                    run = buffer.data;
-                }
-                if (buffer.data < lastPositionHori) {
-
-                    if (run > 0) {
-                        run *= -1;
-                    }
-                } else {
-                    if (run < 0) {
-                        run *= -1;
-                    }
-                }
-                lastPositionHori = buffer.data;
-                angleSamping.runToNewPosition(run * STEP_VERTI);
-                Serial.println("CODE_ANGLE_HORIZONTAL : " + String(lastPositionHori) + " RUN : " + String(run));
+                Serial.println("Buffer Data : " + String(buffer.data));
+                int run = map(buffer.data, -200, 200, 0, MAX_HORI);
+                angleSamping.moveTo(run);
+                angleSamping.runToPosition();
+                Serial.println("CODE_ANGLE_HORIZONTAL : " + String(angleSamping.currentPosition()) + " RUN : " + String(run));
                 /* code */
                 break;
             }
 
             case CODE_ANGLE_VERTICAL: {
-                int runs = 0;
+                float runs = 0;
                 if (buffer.data > MAX_STEP_APPS) {
                     buffer.data = (buffer.data - MAX_STEP_APPS) * -1;
                 }
-                if (lastPositionVerti != 0) {
-                    runs = (lastPositionVerti - buffer.data);
-                } else {
-                    runs = buffer.data;
-                }
-                if (buffer.data < lastPositionVerti) {
-                    if (runs > 0) {
-                        runs *= -1;
-                    }
-                } else {
-                    if (runs < 0) {
-                        runs *= -1;
-                    }
-                }
-                lastPositionVerti = buffer.data;
-                angleAtasBawah.runToNewPosition(runs * STEP_HORI);
-                Serial.println("CODE_ANGLE_VERTICAL : " + String(lastPositionVerti) + " runs : " + String(runs));
+                Serial.println("Buffer Data : " + String(buffer.data));
+                runs = MAX_VERTI - int((70 * (buffer.data + MAX_STEP_APPS)));
+                // Serial.println("Buffer Data : " + String(buffer.data));
+                // int runs = map(buffer.data, -200, 200, MAX_VERTI, 0);
+                // if (runs < 0) {
+                //     runs = map(buffer.data, -200, 200, MAX_VERTI, 0);
+                // }
+
+                angleAtasBawah.moveTo(runs);
+                angleAtasBawah.runToPosition();
+                Serial.println("CODE_ANGLE_VERTI : " + String(angleAtasBawah.currentPosition()) + " RUN : " + String(runs));
                 break;
             }
 
@@ -234,6 +247,18 @@ void actTask(void *pvParameters) {
                 }
                 break;
             }
+            case CODE_MIN_PWM: {
+                MIN_PWM = buffer.data;
+                resetPosition();
+                Serial.println("RESET");
+                break;
+            }
+            case CODE_MAX_PWM: {
+                MAX_PWM = buffer.data;
+                resetPosition();
+                Serial.println("RESET");
+                break;
+            }
             default:
                 break;
             }
@@ -241,71 +266,14 @@ void actTask(void *pvParameters) {
     }
 }
 
-void analogTask(void *pvParameter) {
-    int Analog_X = 0;  // x-axis value
-    int Analog_Y = 0;  // y-axis value
-    int Analog_R = 0;  // r-axis value
-
-    int Analog_X_AVG = 0;  // x-axis value average
-    int Analog_Y_AVG = 0;  // y-axis value average
-    int Analog_R_AVG = 0;  // r-axis value average
-    float tempX = 0;
-    float tempY = 0;
-    float tempR = 0;
-    //----------------------------------------------------------------------------
-    // read the analog 50x, then calculate an average.
-    // they will be the reference values
-    for (int i = 0; i < 50; i++) {
-        tempX += analogRead(Analog_X_pin);
-        delay(10);  // allowing a little time between two readings
-        tempY += analogRead(Analog_Y_pin);
-        delay(10);
-        tempR += analogRead(Analog_R_pin);
-        delay(10);
-    }
-    //----------------------------------------------------------------------------
-    Analog_X_AVG = tempX / 50;
-    Analog_Y_AVG = tempY / 50;
-    Analog_R_AVG = tempR / 50;
-    //-------------------------------
-    Serial.println("Calibration finished");
-    for (;;) {
-        if (isAnalog) {
-            Analog_X = analogRead(Analog_X_pin);
-            Analog_Y = analogRead(Analog_Y_pin);
-            Analog_R = analogRead(Analog_R_pin);
-
-            // if the value is 25 "value away" from the average (midpoint), we allow the update of the speed
-            // This is a sort of a filter for the inaccuracy of the reading
-            if (abs(Analog_X - Analog_X_AVG) > 25) {
-                angleAtasBawah.setSpeed(10 * (Analog_X - Analog_X_AVG));
-            } else {
-                angleAtasBawah.setSpeed(0);
-                Serial.println("0");
-            }
-            //----------------------------------------------------------------------------
-            if (abs(Analog_Y - Analog_Y_AVG) > 25) {
-                angleSamping.setSpeed(10 * (Analog_Y - Analog_Y_AVG));
-            } else {
-                angleSamping.setSpeed(0);
-            }
-            angleSamping.runSpeed();  // step the motor (this will step the motor by 1 step at each loop indefinitely)
-            angleAtasBawah.runSpeed();
-        }
-        vTaskDelay(50);
-    }
-}
-
 void setup() {
     Serial.begin(9600);
     bluetooth.begin(9600);
     bluetooth.listen();
-    pinMode(Analog_X_pin, INPUT);
-    pinMode(Analog_Y_pin, INPUT);
-    pinMode(Analog_R_pin, INPUT);
-    // pinMode(LED_pin, OUTPUT);
     pinMode(SPEED_LEFT, OUTPUT);
     pinMode(SPEED_RIGHT, OUTPUT);
+    pinMode(ISR_PIN_HORI, INPUT_PULLUP);
+    pinMode(ISR_PIN_VERTI, INPUT_PULLUP);
     angleAtasBawah.setMaxSpeed(MAX_SPEED);
     angleAtasBawah.setAcceleration(MAX_ACCEL * 2);
     angleSamping.setMaxSpeed(MAX_SPEED);
@@ -325,24 +293,11 @@ void setup() {
 
     xTaskCreate(
         actTask, "actTask",
-        32,
+        128,
         NULL,
         2,
         NULL);
-
-    // xTaskCreate(
-    //     analogTask, "analogTask",
-    //     128,
-    //     NULL,
-    //     1,
-    //     NULL);
 }
 
 void loop() {
-    // if (isAnalog) {
-    //     // ReadAnalog();
-    //     angleSamping.runSpeed();  // step the motor (this will step the motor by 1 step at each loop indefinitely)
-    //     angleAtasBawah.runSpeed();
-    // }
-    // vTaskDelay(10);
 }
